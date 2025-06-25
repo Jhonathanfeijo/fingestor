2b66f5fae4d9384972234cadb72f0ee7f2b7ea4f
jhonathan <jhonathanfeijomoraes@gmail.com>
2025-06-25 15:57:14 -0400

    feat: adicionando usuario

--- usuario/models.py
+++ usuario/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
     nome = models.TextField()
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=128)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    meta = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
