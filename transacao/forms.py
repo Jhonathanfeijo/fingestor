444a8844625759f7862d883f457dfb65d7a65736
JhonathanFeijo <jhonathanfeijomoraes@gmail.com>
2025-07-06 17:25:34 -0400

    feat: opção de escolher a data

--- transacao/forms.py
+++ transacao/forms.py
@@ -1,16 +1,33 @@
 from django import forms
 from .models import Transacao
 
+ISO_DATE = "%Y-%m-%d"
 
 class TransacaoForm(forms.ModelForm):
+    
+    data = forms.DateField(
+        input_formats=[ISO_DATE],
+        widget=forms.DateInput(
+            attrs={
+                "type": "date",
+                "class": "block w-full rounded-lg bg-slate-800/60 border border-slate-600/50 "
+                         "px-3 py-2 text-white placeholder-gray-400 focus:ring-2 outline-none"
+            },
+            format=ISO_DATE,
+        ),
+        label="Data",
+        required=True,
+    )
+
     class Meta:
         model = Transacao
-        fields = ["tipo_transacao", "valor", "descricao", "categoria"]
+        fields = ["tipo_transacao", "valor", "descricao", "categoria", 'data']
         labels = {
             "tipo_transacao": "Tipo",
             "valor": "Valor (R$)",
             "descricao": "Descrição",
             "categoria": "Categoria",
+            'data':'Data'
         }
         widgets = {
             "tipo_transacao": forms.Select(
@@ -41,3 +58,7 @@ class TransacaoForm(forms.ModelForm):
                 }
             ),
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk and self.instance.data:
+            self.fields["data"].initial = self.instance.data.strftime(ISO_DATE)
--- transacao/models.py
+++ transacao/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from tipo_transacao.models import Tipo_Transacao
 from categoria.models import Categoria
 from usuario.models import Usuario
-
+from django.utils import timezone
 # Create your models here.
 class Transacao(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='transacoes')
     tipo_transacao = models.ForeignKey(Tipo_Transacao, on_delete=models.PROTECT)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     descricao = models.TextField(blank=True)
-    data = models.DateField(auto_now_add=True)  # <-- ALTERADO AQUI
+    data = models.DateField(default = timezone.now)  # <-- ALTERADO AQUI
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
